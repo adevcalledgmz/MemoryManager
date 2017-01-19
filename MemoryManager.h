e987edcb4c24116816c6f875cb7432287d18e4f7
Gmz <g_merchez@opmbx.org>
2017-01-19 12:45:18 +0100

    Update MemoryManager.h
    Adding namespace MemManager and FindProcessId() prototype.

--- MemoryManager.h
+++ MemoryManager.h
@@ -1,18 +1,23 @@
 #include <iostream>
 #include <Windows.h>
 
-class MemoryManager
+namespace MemManager
 {
+    class MemoryManager
+    {
 	public:
-		MemoryManager();
-		int attach(const DWORD& procId);
-		int attach(const std::string& windowName);
-		int attach(std::string processName);
-		void detach();
+	    MemoryManager();
+	    int attach(const DWORD& procId);
+            int attach(const std::string& windowName);
+	    int attach(std::string processName);
+	    void detach();
 
 	private:
-		HANDLE m_processHandle;
-		HWND m_processHWND;
-		DWORD m_processId;
-		bool m_attached;
-};
+	    HANDLE m_processHandle;
+	    HWND m_processHWND;
+	    DWORD m_processId;
+	    bool m_attached;
+    };
+	
+    DWORD FindProcessId(const std::string& processName);	
+}	
