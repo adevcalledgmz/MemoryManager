fee2c960385059fa768bbd2d5de241d1c6d82f59
Gmz <g_merchez@opmbx.org>
2017-01-19 12:58:48 +0100

    Update MemoryManager.cpp

--- MemoryManager.cpp
+++ MemoryManager.cpp
@@ -2,71 +2,86 @@
 
 namespace MemManager
 {
-	int MemoryManager::attach(const DWORD& procId)
+    int MemoryManager::attach(const DWORD& procId)
+    {
+	if (procId == 0)
 	{
-		if (procId == 0)
-		{
-			return -1;
-		}
-
-		m_processHandle = OpenProcess(PROCESS_ALL_ACCESS, FALSE, procId);
-
-		if (m_processHandle == NULL)
-		{
-			return -1;
-		}
-
-		return 0;
+	    return -1;
 	}
 
-	int MemoryManager::attach(const std::string& windowName)
+	m_processHandle = OpenProcess(PROCESS_ALL_ACCESS, FALSE, procId);
+
+	if (m_processHandle == NULL)
 	{
-		m_processHWND = FindWindowA(0, windowName.c_str());
-
-		if (m_processHWND == NULL)
-		{
-			return -1;
-		}
-
-		GetWindowThreadProcessId(m_processHWND, &m_)
-
-
-	//Todo finish this function
+	    return -1;
 	}
 
-	void MemoryManager::detach()
+	m_attached = true;    
+	    
+	return 0;
+    }
+
+    int MemoryManager::attach(const std::string& windowName)
+    {
+	m_processHWND = FindWindowA(0, windowName.c_str());
+
+	if (m_processHWND == NULL)
 	{
-		CloseHandle(m_processHandle);
+	    return -1;
 	}
+
+        GetWindowThreadProcessId(m_processHWND, &m_)
+
+        m_processHandle = OpenProcess(PROCESS_ALL_ACCESS, FALSE, m_processId);
+
+    	if(m_processHandle == NULL)
+    	{
+            return -1;
+    	}
+	    
+	m_attached = true;
 	
-	WORD FindProcessId(const std::string& processName)
+	return 0;	
+    }
+
+    void MemoryManager::detach()
+    {
+	if(m_attached)
+        {
+            CloseHandle(m_processHandle);
+            m_processId = 0;
+            m_attached = false;
+        }
+    }
+	
+    WORD FindProcessId(const std::string& processName)
+    {
+	PROCESSENTRY32 processInfo;
+	processInfo.dwSize = sizeof(processInfo);
+
+	HANDLE processesSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, NULL);
+		
+	if(processesSnapshot == INVALID_HANDLE_VALUE)
+	    return 0;
+
+	Process32First(processesSnapshot, &processInfo);
+		
+        if(!processName.compare(processInfo.szExeFile))
 	{
-	    PROCESSENTRY32 processInfo;
-	    processInfo.dwSize = sizeof(processInfo);
+            CloseHandle(processesSnapshot);
+	    return processInfo.th32ProcessID;
+	}
 
-	    HANDLE processesSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, NULL);
-		
-	    if (processesSnapshot == INVALID_HANDLE_VALUE)
-		return 0;
-
-	    Process32First(processesSnapshot, &processInfo);
-		
-	    if (!processName.compare(processInfo.szExeFile))
+	while(Process32Next(processesSnapshot, &processInfo))
+	{
+            if (!processName.compare(processInfo.szExeFile))
 	    {
 		CloseHandle(processesSnapshot);
 		return processInfo.th32ProcessID;
 	    }
+	}
 
-	    while (Process32Next(processesSnapshot, &processInfo))
-	    {
-		if (!processName.compare(processInfo.szExeFile))
-		{
-		    CloseHandle(processesSnapshot);
-		    return processInfo.th32ProcessID;
-		}
-	    }
-
-	    CloseHandle(processesSnapshot);
-	    return 0;
+	CloseHandle(processesSnapshot);
+	return 0;
     }	
 }	
